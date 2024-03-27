1ba319be86b417c97aed30d538b300af7c911e90
James <mirageapache@gmail.com>
2024-03-27 22:22:12 +0800

    [feat]新增CryptoJS

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const user = require('./modules/user')
-const article = require('./modules/article')
-const comment = require('./modules/comment')
-const login = require('./modules/login')
+const article = require("./modules/article");
+const comment = require("./modules/comment");
+const login = require("./modules/login");
+const user = require("./modules/user");
+const utility = require("./modules/utility");
 
-router.use('/user', user)
+router.use("/article", article);
+router.use("/comment", comment);
+router.use("/login", login);
+router.use("/user", user);
+router.use("/utility", utility);
 
-router.use('/article', article)
-
-router.use('/comment', comment)
-
-router.use('/login', login)
-
-module.exports = router
+module.exports = router;
