e57e540abc8e1ee97d98d67c6dd2615562db9fe3
jameswang14 <james.wang@settour.com.tw>
2024-04-17 16:19:43 +0800

    [optimize] 優化 authorization寫法

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,18 @@
 const jwt = require("jsonwebtoken");
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
+/** authToken 驗證 */
 const authorization = (req, res, next) => {
   const token = req.header("Authorization").split(" ")[1];
-  console.log(token);
   if (!token) {
     return res.status(401).json({ message: "No token provided" });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      console.error('JWT驗證失敗:', err);
-      return res.status(401).json({ message: "Unauthorized" , error: err});
-    } else {
-      console.log('JWT驗證成功，解析結果:', decoded);
+    if (decoded && req.params.id === decoded.userId) {
       next();
+    } else {
+      return res.status(401).json({ message: "Unauthorized" , error: err});
     }
   });
 };
