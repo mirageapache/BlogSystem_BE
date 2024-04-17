495287c913cbf6e376a257255e530539c33ecfa4
jameswang14 <james.wang@settour.com.tw>
2024-04-17 10:14:20 +0800

    [bugfix]修正 authorization jwt verify

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,22 +2,21 @@ const jwt = require("jsonwebtoken");
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const authorization = (req, res, next) => {
-  const token = req.header("Authorization");
+  const token = req.header("Authorization").split(" ")[1];
+  console.log(token);
   if (!token) {
     return res.status(401).json({ message: "No token provided" });
   }
 
-  console.log(req.params.id);
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  console.log(decoded);
-  try {
-    // 驗證 token
-    // if ((req.params.id = decoded.id)) {
-    //   next();
-    // } else {
-    //   return res.status(401).json({ message: "Unauthorized" });
-    // }
-  } catch (error) {}
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      console.error('JWT驗證失敗:', err);
+      return res.status(401).json({ message: "Unauthorized" , error: err});
+    } else {
+      console.log('JWT驗證成功，解析結果:', decoded);
+      next();
+    }
+  });
 };
 
 module.exports = { authorization };
