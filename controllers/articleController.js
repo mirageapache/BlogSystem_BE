1e3019e6b02fe8455da23eefd1b0577bc1a302a3
jameswang14 <james.wang@settour.com.tw>
2024-07-31 15:16:36 +0800

    [feat] create article function

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -14,7 +14,56 @@ const articleController = {
       res.status(500).json({ message: error.message });
     }
   },
-  /** 取得特定文章 */
+  /** 取得搜尋文章
+   * @param searchString 搜尋字串
+   * @param authorId 作者id
+   */
+  getSearchArticleList: async (req, res) => {
+    const { searchString, authorId } = req.body;
+    let variable = {};
+
+    if (!isEmpty(searchString) && !isEmpty(authorId)) {
+      variable = {
+        $or: [
+          { content: new RegExp(searchString, "i") },
+          { hashTags: new RegExp(searchString, "i") },
+          { author: authorId },
+        ],
+      };
+    } else if (!isEmpty(searchString)) {
+      variable = {
+        $or: [
+          { content: new RegExp(searchString, "i") },
+          { hashTags: new RegExp(searchString, "i") },
+        ],
+      };
+    } else if (!isEmpty(authorId)) {
+      variable = { author: authorId };
+    }
+
+    try {
+      const articles = await Article.find(variable)
+        .sort({ createdAt: -1 })
+        .populate("author", {
+          _id: 1,
+          account: 1,
+          name: 1,
+          avatar: 1,
+          bgColor: 1,
+        })
+        .populate({
+          path: "likedByUsers",
+          select: "_id account name avatar bgColor",
+        })
+        .populate("comments")
+        .lean()
+        .exec();
+      res.status(200).json(articles);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+  /** 取得文章詳細資料 */
   getArticleDetail: async (req, res) => {
     try {
       const article = await Article.findById(req.params.id)
@@ -31,15 +80,22 @@ const articleController = {
   },
   /** 新增文章 */
   createArticle: async (req, res) => {
-    const { author, title, content, subject, tags } = req.body;
+    const { author, title, content, subjects, hashTags } = req.body;
+    const subjectArr = !isEmpty(subjects) ? JSON.parse(subjects) : [];
+    const hashTagArr = !isEmpty(hashTags) ? JSON.parse(hashTags) : [];
+    const articleImage = req.file || {};
+    const filePath = !isEmpty(articleImage)
+      ? await imgurFileHandler(articleImage)
+      : null; // imgur圖片檔網址(路徑)
+
     try {
       const newArticle = await Article.create({
         author,
         title,
         content,
         status: 0,
-        subject,
-        tags,
+        subjects: subjectArr,
+        hashTags: hashTagArr,
         createdAt: moment.tz(new Date(), "Asia/Taipei").toDate(),
         likedByUsers: [],
         comments: [],
@@ -50,7 +106,7 @@ const articleController = {
     }
   },
 
-  /** 更新文章 */
+  /** 編輯(更新)文章 */
   updateArticle: async (req, res) => {
     try {
       const updatedArticle = await Article.findByIdAndUpdate(
@@ -73,6 +129,44 @@ const articleController = {
       res.status(500).json({ message: error.message });
     }
   },
+  /** 喜歡/取消喜歡 文章
+   * @param articleId 文章Id
+   * @param userId 使用者Id
+   * @param action true / false
+   */
+  toggleLikeArticle: async (req, res) => {
+    const { articleId, userId, action } = req.body;
+
+    try {
+      const articleData = await Article.findById(articleId);
+      const likeList = articleData.likedByUsers;
+      let newLikeList = likeList.map((obj) => obj.toString());
+
+      if (action) {
+        // like action
+        if (!newLikeList.includes(userId)) newLikeList.push(userId);
+      } else {
+        // unlike action
+        const rmIndex = newLikeList.indexOf(userId);
+        if (rmIndex !== -1) newLikeList.splice(rmIndex, 1);
+      }
+
+      // 回寫至DB
+      const updateResult = await Article.findByIdAndUpdate(
+        articleId,
+        { likedByUsers: newLikeList },
+        { new: true }
+      ).populate({
+        path: "likedByUsers",
+        select: "_id account name avatar bgColor",
+      });
+
+      return res.status(200).json({ message: "succeess", updateResult });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  },
+
 };
 
 module.exports = articleController;
--- routes/modules/article.js
+++ routes/modules/article.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const articleController = require('../../controllers/articleController');
-
+const { uploadFile } = require("../../middleware/fileUtils");
+const { authorization } = require('../../middleware/auth');
 
 /** 取得所有文章 */
 router.get('/', articleController.getAllArticle);
@@ -10,12 +11,19 @@ router.get('/', articleController.getAllArticle);
 router.post('/:id', articleController.getArticleDetail);
 
 /** 新增文章 */
-router.post('/create', articleController.createArticle);
+router.post('/create', authorization, uploadFile.single("articleImage"), articleController.createArticle);
 
 /** 更新文章 */
-router.patch('/:id', articleController.updateArticle);
+router.patch('/:id', authorization, uploadFile.single("articleImage"), articleController.updateArticle);
 
 /** 刪除文章 */
-router.delete('/:id', articleController.deleteArticle);
+router.delete('/:id', authorization, articleController.deleteArticle);
+
+/** 喜歡/取消喜歡文章 */
+router.patch('/toggleLikeAction/:id', authorization, articleController.toggleLikeArticle);
+
+/** 收藏/取消收藏文章 */
+// router.patch('/toggleStoreAction/:id', authorization, articleController.toggleStorePost);
+
 
 module.exports = router;
