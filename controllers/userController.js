08ee08db813dbf67ab1bdf54e82edea390a5a617
James <mirageapache@gmail.com>
2024-09-22 11:15:19 +0800

    [bugfix] fix getSearchUserList no following error

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,7 +73,12 @@ const userController = {
         })
         .lean()
         .exec();
-      if (isEmpty(follows)) return res.status(200).json(users); // 沒有followList(表示未追縱任何人)，則直接回傳userList
+      if (isEmpty(follows))
+        return res.status(200).json({
+          userList: users,
+          nextPage,
+          totalUser: total,
+        }); // 沒有followList(表示未追縱任何人)，則直接回傳userList
 
       // 將追蹤清單轉換為哈希表(Object)
       const followsMap = follows.reduce((acc, follow) => {
